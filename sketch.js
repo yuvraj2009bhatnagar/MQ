48edf46e8d43ec53566d8445e901db4274b0a5ba
yuvraj2009bhatnagar <73655765+yuvraj2009bhatnagar@users.noreply.github.com>
2021-06-05 20:01:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();1
+  quiz.start();
+  quiz.play();
+}
+
+
+function draw(){
+  background("pink");
+  if(contestantCount === 2){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    quiz.play();
+  }
+}
